dd8365792889674c122eac14cb02f648907912c0
Biruk Erjamo <birukerjamo@gmail.com>
2024-09-14 09:58:12 +0300

    Add full-screen view, model options, metadata, download button, text overlay, and social media sharing.

---
+++ src/constants/models.ts
@@ -0,0 +1,12 @@
+export const models = [
+    {
+        id: 1,
+        value: "Firework",
+        label: "Firework",
+    },
+    {
+        id: 2,
+        value: "Dalle",
+        label: "Dalle"
+    },
+]
