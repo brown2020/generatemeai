9865623273aad56c52ad5b8dc5f7aefe5854f6ba
Steve Brown <brown2020@gmail.com>
2024-09-29 15:42:08 -0700

    Remove "nature" from promptdesign, replaced by imageCategory headshot

--- src/utils/promptUtils.ts
+++ src/utils/promptUtils.ts
@@ -12,11 +12,7 @@ export const generatePrompt = (
   let promptDesign;
 
   // Start with the basic prompt based on the visual
-  if (colorScheme == "Nature") {
-    promptDesign = `Create a close-up, color photograph of a ${visual}, capturing a lifelike portrayal of the subject. The image should feature a single, vibrant living thing figure facing forward, including the entire head and body with some margin. Demeanor: Friendly. Background: Soft gradient. Age/Stage: Young by default if not provided in the first sentence of this prompt. Appearance/Details: Natural and realistic features, including face, hair, and clothing that reflect a youthful, authentic look. Please avoid generating a statue or any non-nature representation; focus solely on a living, breathing living things (human, animation, plants or other) likeness, emphasizing youthful appearance by default if not provided on the first sentence of this prompt.`;
-  } else {
-    promptDesign = visual;
-  }
+  promptDesign = visual;
 
   // Apply the artistic style, if provided
   if (artStyle) {
@@ -27,7 +23,7 @@ export const generatePrompt = (
   promptDesign += `\n\nThe image should be without words. No text or logos.`;
 
   // Apply the color scheme, if provided and valid
-  if (colorScheme && colorScheme !== "None" && colorScheme !== "Nature") {
+  if (colorScheme && colorScheme !== "None") {
     promptDesign += `\n\nUse this for color scheme: ${colorScheme}`;
   }
 
