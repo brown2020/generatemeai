fbbb8cd66d8ea46b94bb0e1ad6ae07de70f25e9d
Biruk Erjamo <birukerjamo@gmail.com>
2024-10-10 05:30:30 +0300

    improved suggesting tags prompt

--- src/actions/suggestTags.ts
+++ src/actions/suggestTags.ts
@@ -16,8 +16,23 @@ export const suggestTags = async (freestyle: string, videoScript: string, color:
         const data = {
             model: 'gpt-4',
             messages: [
-                { role: 'system', content: 'For all response reply just the answer without giving any description.' },
-                { role: 'user', content: `Using this prompt that image created with\n\nthe prompt: ${freestyle} ${videoScript && `videoScript: ${videoScript}`} ${color && `color: ${color}`} ${lighting && `lighting: ${lighting}`} ${style && `style: ${style}`} ${imageCategory && `ImageCategory: ${imageCategory}`}\n\nSuggest tags for the image. It shouldn't be from this list: ${tags}. Please list the tags in this format: separate all tags with commas, that's it, nothing else, and don't use a full stop at the end. Provide only suggest 6, no explanation.\n\n` }
+                { 
+                    role: 'system', 
+                    content: 'For all response reply just the answer without giving any description.' 
+                },
+                { 
+                    role: 'user', 
+                    content: `Using this prompt that image created with\n\n` +
+                        `the prompt: ${freestyle}` +
+                        `${videoScript ? ` videoScript: ${videoScript}` : ''}` +
+                        `${color && color !== 'None' ? ` color: ${color}` : ''}` +
+                        `${lighting && lighting !== 'None' ? ` lighting: ${lighting}` : ''}` +
+                        `${style ? ` style: ${style}` : ''}` +
+                        `${imageCategory ? ` ImageCategory: ${imageCategory}` : ''}` +
+                        `\n\nSuggest tags for the image. It shouldn't be from this list: ${tags}. ` +
+                        `Please list the tags in this format: separate all tags with commas, ` +
+                        `that's it, nothing else, and don't use a full stop at the end. Provide only suggest 6, no explanation.\n\n` 
+                }
             ],
             max_tokens: 200,
             temperature: 0.7,
