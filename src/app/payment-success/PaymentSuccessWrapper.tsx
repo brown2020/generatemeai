258b115b5c2a9a590ea62a757315b933540c8f1c
Steve Brown <brown2020@gmail.com>
2026-04-13 15:04:02 -0700

    fix: wrap useSearchParams in Suspense boundary (#47)
    * fix: add PaymentSuccessWrapper with useSearchParams
    
    * fix: wrap useSearchParams page in Suspense boundary

---
+++ src/app/payment-success/PaymentSuccessWrapper.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import PaymentSuccessPage from "@/components/PaymentSuccessPage";
+import { useSearchParams } from "next/navigation";
+
+export default function PaymentSuccessWrapper() {
+  const searchParams = useSearchParams();
+  const payment_intent = searchParams.get("payment_intent") || "";
+
+  return <PaymentSuccessPage payment_intent={payment_intent} />;
+}
